4c9e454d9426ecbe322185f72f695fe5f4e13b7a
Kunj Arora <kunj57932@gmail.com>
2023-05-06 18:06:52 +0530

    copy constructor

--- Lecture 26 (6MAY)/objectOrientedProgramming.cpp
+++ Lecture 26 (6MAY)/objectOrientedProgramming.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 class Car
 {   
+
+public:
+
     string color;
     int wheelSize;
     string modelNo;
     int len;
     int speed;
-public:
 
     Car()
     {
@@ -24,6 +26,15 @@ public:
         color=COLOR;
         len=LEN;
         speed=SPEED;
+    }
+
+    Car(Car &X)
+    {
+        cout<<"copy constructor invoked"<<endl;
+        wheelSize=X.wheelSize;
+        color=X.color;
+        len=X.len;
+        speed=X.speed;
     }
 
     void accelerate()
@@ -40,6 +51,7 @@ public:
     {
         return color;
     }
+
 };
 
 int main()
@@ -47,6 +59,8 @@ int main()
 
     Car BMW(18,"Black",4300,0);
 
+    // ObjectKaNaam.propertyName;
+    // ObjectKaNaam.funcName();
     cout<<BMW.getColor()<<endl;
     BMW.changeColor("MatteBlack");
     // BMW.color="Pink";
@@ -55,11 +69,20 @@ int main()
     // cout<<BMW.color<<endl;
 
 
-    Car Audi;
+    // Car Audi;
+    
+    Car Mercedes=BMW;
+    // Car Mercedes(BMW);
 
-    BMW.changeColor(Audi.getColor());
+    cout<<"Created Mercedes"<<endl;
+    cout<<BMW.getColor()<<endl;
+    cout<<Mercedes.getColor()<<endl;
 
-
+    
+    int x=10;
+    // int y=x;
+    int y(x);
+    cout<<"y: "<<y<<endl;
 
     return 0;
 }
